8c8c1d25adb44b5e3b290a04c6f314016b10b6e4
harshraj172 <64323122+harshraj172@users.noreply.github.com>
2021-05-06 00:28:44 +0530

    Update similarity.py

--- similarity.py
+++ similarity.py
@@ -2,13 +2,13 @@
 from scipy.spatial import distance
 
 def similarity_score(ref_image, selfie_image, device):
-    '''
+    """
     Given a selfie image and the reference image to compare.
     Returns the similarity score.
     ref_image: Image stored with company for reference.
     selfie_image: Image taken by employee to mark his presence.
     device : "cuda" or "cpu" device.
-    '''
+    """
     # for ref_image
     ref_image_tensor = T.ToTensor()(ref_image)
     ref_image_tensor = ref_image_tensor.unsqueeze(0)
